c4883da7960a48a3c07d279535983b8df65b5259
nico17022001 <nico17022001boolean@gmail.com>
2023-05-09 15:31:36 +0200

    aggiunta section reviews

--- src/components/partials/section-reviews.vue
+++ src/components/partials/section-reviews.vue
@@ -7,14 +7,70 @@ export default {
 
 <template>
   <div id="main-wrapper">
+    <div id="main-container">
+      <!-- CONTENITORE DEL TITOLO DI SEZIONE -->
+      <div id="container">
+        <div id="what"><span>WHAT PEOPLE SAY</span></div>
+        <div id="title"><h2>Reviews</h2></div>
+      </div>
+      <!-- CONTENITORE DELLE CARDS -->
+      <div id="reviews-box">
+        <!-- CARD -->
+        <div class="card">
+          <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos possimus amet modi optio sapiente distinctio, quasi tempora nam! Voluptates, quo! Amet aliquam numquam, ipsam accusamus dolore alias ipsum voluptatibus commodi.</div>
+          <div class="card-img">Immagine</div>
+          <div class="card-name"><span>15$</span></div>
+        </div>
 
+                <!-- CARD -->
+                <div class="card">
+          <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos possimus amet modi optio sapiente distinctio, quasi tempora nam! Voluptates, quo! Amet aliquam numquam, ipsam accusamus dolore alias ipsum voluptatibus commodi.</div>
+          <div class="card-img">Immagine</div>
+          <div class="card-name"><span>15$</span></div>
+        </div>
+
+                <!-- CARD -->
+        <div class="card">
+          <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos possimus amet modi optio sapiente distinctio, quasi tempora nam! Voluptates, quo! Amet aliquam numquam, ipsam accusamus dolore alias ipsum voluptatibus commodi.</div>
+          <div class="card-img">Immagine</div>
+          <div class="card-name"><span>15$</span></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
   #main-wrapper{
-    height: 600px;
     background-color: cornflowerblue;
     width: 100%;
+    display: flex;
+    justify-content: center;
+  }#main-container{
+    margin-top:400px ;
+    background-color: bisque;
+    width: 80%;
+  }#container{
+    display: flex;
+    flex-direction:column ;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #reviews-box{
+    height: 200px;
+    width: 100%;
+    background-color: yellow ;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0px 200px;
+  }.card{
+    width: calc(100% / 3);
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
   }
 </style>
