8bd60c71573260cc89c829d486108d9afb1a1a80
nico17022001 <nico17022001boolean@gmail.com>
2023-05-09 12:46:06 +0200

    aggiunta sezione pro barbers

--- src/components/partials/section-pro-barbers.vue
+++ src/components/partials/section-pro-barbers.vue
@@ -6,9 +6,47 @@ export default {
 </script>
 
 <template>
-  
+  <!-- CONTENITORE CON SFONDO -->
+  <div id="main-wrapper">
+    <!-- CONTENITORE INFO SEZIONE -->
+    <div id="container">
+      <!-- ELEMENTI SEZIONE -->
+      <div id="award-container">AWARD WINNING</div>
+      <div id="pro-container">Pro barbers</div>
+      <div id="meet-container">MEET YOUR NEW BARBER</div>
+    </div>
+    <!-- CONTENITORE SVG PER SFONDO -->
+    <div id="svg-container"></div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+  #main-wrapper{
+    height: 500px;
+    background-color: aqua;
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }#container{
+    height: 100%;
+    width: 650px;
+    background-color: blanchedalmond;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }#award-container{
+    background-color: coral;
+  }#pro-container{
+    background-color: cadetblue;
+  }#meet-container{
+    background-color: brown;
+  }
 
+#svg-container{
+  background-color: white;
+  height: 100px;
+  width: 100%;
+}
 </style>
